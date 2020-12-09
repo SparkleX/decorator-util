551fc9fe8d589dbb7669450f6906ca1131643657
SparkleX <xufei.sun@sap.com>
2020-12-09 09:19:15 +0800

    dev

--- src/DecoratorUtil.ts
+++ src/DecoratorUtil.ts
@@ -1,7 +1,6 @@
 
 export interface CallbackParam {
 	param: string;
-	_this: object;
 	target: object;
 	propertyKey: string;
 	descriptor: PropertyDescriptor;
@@ -9,19 +8,14 @@ export interface CallbackParam {
 
 
 type  CallbackFunction = (x: CallbackParam, ...params: any[]) => any;
-
-type  DecoratorStringFunction = (x: string,b: string) => any;
-
 export class DecoratorUtil {
 	public static define(callback: CallbackFunction): any {
 		return function (p: string,b: string) {
-			console.debug(b);
 			const that = this;
 			return function (target: object, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor {
-				console.debug(b);
 				const param: CallbackParam = {
 					param: p,
-					_this: this,
+					//_this: this,
 					target: target,
 					propertyKey: propertyKey,
 					descriptor: descriptor,
@@ -36,38 +30,3 @@ export class DecoratorUtil {
 	}
 }
 
-const callback = async function (x: CallbackParam, ...params: any[]): Promise<any> {
-	console.debug(x);
-	console.debug(params);
-	console.debug(this);
-	return 1;
-}
-
-
-const aaa = DecoratorUtil.define(callback);
-
-const sqlImpl = function (sql?: string) {
-	console.debug(this);
-	return function (target: object, propertyKey: string, descriptor: PropertyDescriptor): PropertyDescriptor {
-		descriptor.value = async function (...params: any[]): Promise<any> {
-			console.debug(this);
-			return null;
-		};
-		return descriptor;
-	};
-}
-
-//const sql = sqlImpl.bind("222");
-
-
-export class TestRepo {
-	public aaa = 100;
-	@aaa("select *","222")
-	public test(a:string): void {
-		console.debug("ERROR");
-	}
-
-}
-
-const oTestRepo = new TestRepo();
-oTestRepo.test("123");
--- src/__test__/DecoratorUtil.spec.ts
+++ src/__test__/DecoratorUtil.spec.ts
@@ -1,24 +1,44 @@
 import { DecoratorUtil, CallbackParam } from "../DecoratorUtil";
 
-async function callback(x: CallbackParam, params: any[]): Promise<any> {
+/*async function callback(x: CallbackParam, params: any[]): Promise<any> {
 	console.debug(x);
 	console.debug(params);
 	console.debug(this);
 	return 1;
-}
+}*/
 
-
+const callback = jest.fn();
 const sql: (sql: string) => any = DecoratorUtil.define(callback);
-
 
 class TestRepo {
 	@sql("select * from table")
-	async query(param: string): Promise<any> {
+	query(p1: any, p2: any): any {
+		throw -1;
+	}
+
+	@sql("select * from table")
+	async queryAsync(p1: any, p2: any): Promise<any> {
 		throw -1;
 	}
 }
 
-test("", ()=>{
+test("sync call", () => {
+	callback.mockReturnValueOnce("return");
 	const oTestRepo = new TestRepo();
-	oTestRepo.query("p1");
-}) 
+	const ret = oTestRepo.query("p1", "p2");
+	expect(ret).toBe("return");
+	expect(callback.mock.calls.length).toBe(1);
+	expect(callback.mock.calls[0][0].param).toStrictEqual("select * from table");
+	expect(callback.mock.calls[0][1]).toStrictEqual(["p1", "p2"]);
+});
+
+
+test("async call", async () => {
+	callback.mockResolvedValue("return");
+	const oTestRepo = new TestRepo();
+	const ret = await oTestRepo.queryAsync("p1", "p2");
+	expect(ret).toBe("return");
+	expect(callback.mock.calls.length).toBe(1);
+	expect(callback.mock.calls[0][0].param).toStrictEqual("select * from table");
+	expect(callback.mock.calls[0][1]).toStrictEqual(["p1", "p2"]);
+});
